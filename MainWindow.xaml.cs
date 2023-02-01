b52b95af19c287854c109c8862deb9f7c91824ba
forziking <sdjglk@mail.ru>
2023-02-01 11:47:41 +0300

    Nishuganovo

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ЛР1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Background = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Background = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new Pages.ZadT());
+        }
+    }
+}
---
+++ Pages/ZadT.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ЛР1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ZadT.xaml
+    /// </summary>
+    public partial class ZadT : Page
+    {
+        public ZadT()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "Привет";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "До свидания";
+        }
+    }
+}
